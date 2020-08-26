13cadf4f16af3f98591764aebd9ec8b2d15d9a88
binaf <ousmanehaidara26@gmail.com>
2020-08-26 13:36:19 +0200

    finalisation single.js

--- src/components/Single.js
+++ src/components/Single.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 const Single = (props) => {
     return ( 
-        <div className="col s12 l6">
+        <li className="col s12 l6">
             <div className="card">
                 <div className="card-image">
-                    <img src="images/sample-1.jpg" alt={props.item.title}/>
+                    <img src="oushdr.jpeg" alt={props.item.title}/>
                     <span className="card-title">{props.item.prenom} {props.item.nom} - {props.item.compagnie}</span>
                 </div>
                 <div className="card-content">
-                    
+                    <span className="card-title"> {props.item.email} </span>
+                    <div className="row">
+                        <div className="col l6 center">
+                            <blockquote> <h5> {props.item.notes} </h5> </blockquote>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>    
+        </li>    
      );
 }
  
