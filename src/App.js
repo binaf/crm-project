afccf9ac0ac93e27f6a2a97db557b093809fd93a
binaf <ousmanehaidara26@gmail.com>
2020-08-26 11:20:15 +0200

    Configuration de firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCqM7m9yCFLEP0Ha4owxO3wz4ZBRtzhDgU",
+      authDomain: "crm-project-c614d.firebaseapp.com",
+      databaseURL: "https://crm-project-c614d.firebaseio.com",
+      projectId: "crm-project-c614d",
+      storageBucket: "crm-project-c614d.appspot.com",
+      messagingSenderId: "61175138172",
+      appId: "1:61175138172:web:0e16e73a15e9ecdb4986f9",
+      measurementId: "G-V3SXVD8EW2",
+    })
+  }
+
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +38,9 @@ function App() {
       </header>
     </div>
   );
+
+  }
+
 }
 
 export default App;
