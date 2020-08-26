5dde5f3349b66859c8c5d924dcaa871cad70d578
binaf <ousmanehaidara26@gmail.com>
2020-08-26 14:52:37 +0200

    ajout button pour supprimer

--- src/components/Single.js
+++ src/components/Single.js
@@ -5,19 +5,22 @@ const Single = (props) => {
         <li className="col s12 l6">
             <div className="card">
                 <div className="card-image">
-                    <img src="oushdr.jpeg" alt={props.item.title}/>
+                    <img src="./oushdr.jpeg" alt={props.item.title}/>
                     <span className="card-title">{props.item.prenom} {props.item.nom} - {props.item.compagnie}</span>
                 </div>
                 <div className="card-content">
-                    <span className="card-title"> {props.item.email} </span>
+                    <span className="card-title">{props.item.email}</span>
                     <div className="row">
                         <div className="col l6 center">
-                            <blockquote> <h5> {props.item.notes} </h5> </blockquote>
+                            <blockquote><h5>{props.item.notes}</h5></blockquote>
                         </div>
                     </div>
                 </div>
+                <div className="card-action">
+                    <button className="waves-effect waves-light btn red darken-4"><i class="material-icons">delete</i></button>
+                </div>
             </div>
-        </li>    
+        </li>
      );
 }
  
