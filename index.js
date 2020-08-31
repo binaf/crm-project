783ce77fade56e3618ff5ab516de65d81629c7dd
binaf <ousmanehaidara26@gmail.com>
2020-08-31 16:50:59 +0200

    insertion du dossier build dans le depot

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'app-react', 'build', 'index.html'));
 });
 // if not in production use the port 8080
-//const PORT = process.env.PORT || 8080;
-//console.log('Le serveur tourne sur le port:',PORT);
-//app.listen(PORT);
-app.listen(8080, '0.0.0.0');
+const PORT = process.env.PORT || 8080;
+console.log('Le serveur tourne sur le port:',PORT);
+app.listen(PORT, '0.0.0.0');
+//app.listen(8080, '0.0.0.0');
